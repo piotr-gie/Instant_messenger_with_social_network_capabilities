b81de5f78dec0a4402f8f5a3b88f46553aa379da
Flaysen <kamiltuszkiewicz@gmail.com>
2021-03-14 20:41:25 +0100

    login page & navbar layout

--- InstantMessengerFrontend/src/app/app.module.ts
+++ InstantMessengerFrontend/src/app/app.module.ts
@@ -4,23 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './nav/navbar/navbar.component';
+import { NavbarComponent } from './components/nav/navbar/navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MaterialModule } from './material.module';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [	
     AppComponent,
     NavbarComponent,
-      LoginComponent
+    LoginComponent,
+    HomeComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    MatToolbarModule
+    MatToolbarModule,
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- InstantMessengerFrontend/src/app/components/login/login.component.ts
+++ InstantMessengerFrontend/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
+
   }
 
 }
