85b5fe7a9aa8e68d964bcc8dbf04cdc0d2896c69
Flaysen <kamiltuszkiewicz@gmail.com>
2021-04-15 10:03:22 +0200

    files download

--- InstantMessengerFrontend/src/app/components/shared/rich-text-box/rich-text-box.component.ts
+++ InstantMessengerFrontend/src/app/components/shared/rich-text-box/rich-text-box.component.ts
@@ -2,6 +2,8 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { TextBoxType } from 'src/app/enums/message-box-type.enum';
 import { Message } from 'src/app/helpers/message';
+import { FileDownloadService } from 'src/app/services/file-download.service';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-rich-text-box',
@@ -98,6 +100,11 @@ export class RichTextBoxComponent implements OnInit {
     if (index >= 0) {
       this.uploadedFiles.splice(index, 1);
     }
+  }
+
+  downloadFile() {
+    const fileToDowload = this.uploadedFiles[0];
+    saveAs(new Blob([fileToDowload], {type: ''}), fileToDowload.name);   
   }
 }
 
