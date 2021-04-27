ffe5066add2cd6573434cf1e4dddc65742f2f332
dzeju <dzeju@users.noreply.github.com>
2021-04-27 12:35:37 +0200

    navbar sicks at the top, search bar

--- InstantMessengerFrontend/src/app/components/nav/navbar/navbar.component.ts
+++ InstantMessengerFrontend/src/app/components/nav/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  encapsulation : ViewEncapsulation.None
 })
 export class NavbarComponent implements OnInit {
   authService: AuthService;
