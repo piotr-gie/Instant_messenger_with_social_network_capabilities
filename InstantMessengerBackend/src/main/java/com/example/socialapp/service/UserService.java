f8a1d87550f018c1742f3fd315e4736150e9c556
Adam Skowronski <236036@student.pwr.edu.pl>
2021-05-03 22:41:32 +0200

    crud for user

--- InstantMessengerBackend/src/main/java/com/example/socialapp/service/UserService.java
+++ InstantMessengerBackend/src/main/java/com/example/socialapp/service/UserService.java
@@ -2,17 +2,79 @@ package com.example.socialapp.service;
 
 import com.example.socialapp.model.User;
 import com.example.socialapp.model.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import com.example.socialapp.model.*;
+
+import java.util.List;
+import java.util.Optional;
+
 
 @Service
-public class UserService {
-    UserRepository userRepository;
+public class UserService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    public User add(User user) {
+        return userRepository.save(user);
+    }
+
     public User getById(int userId){
         return userRepository.findById(userId).orElseThrow();
     }
+
+
+
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    public User updateById(User user) {
+        userRepository.updateById(user.getFirstName(), user.getLastName(),
+                user.getMail(), user.getPassword(),
+                user.getAboutMe(), user.getPhone(),
+                user.getBirthday(), user.getCity(),
+                user.getGender(), user.getProfileImage(),
+                user.isActive(),user.getRoles(),
+                user.getId());
+        return user;
+    }
+
+    public User deleteById(int id) {
+        Optional<User> optional = userRepository.findById(id);
+        if(optional.isPresent()) {
+            userRepository.deleteById(id);
+            return optional.get();
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String mail) throws UsernameNotFoundException {
+        Optional<User> user = Optional.ofNullable(userRepository.getByMail(mail));
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + mail));
+
+        com.example.socialapp.model.UserDetails userDetails = new com.example.socialapp.model.UserDetails(user.get());
+
+        return userDetails;
+    }
+
+//    public User authentication(String mail, String password) {
+//        User user = userRepository.getByMail(mail);
+//        if(user.getPassword().equals(ShaHashing.encrypted(password)))
+//            return user;
+//        else
+//            return null;
+//    }
+
 }
