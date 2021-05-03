ab4b04d6cf945641c9c3e52996bfefec0360b939
Adam Skowronski <236036@student.pwr.edu.pl>
2021-05-03 22:41:32 +0200

    crud for user

--- InstantMessengerBackend/src/main/java/com/example/socialapp/SocialAppApplication.java
+++ InstantMessengerBackend/src/main/java/com/example/socialapp/SocialAppApplication.java
@@ -2,6 +2,9 @@ package com.example.socialapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SocialAppApplication {
@@ -9,5 +12,7 @@ public class SocialAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SocialAppApplication.class, args);
 	}
+
+
 
 }
--- InstantMessengerBackend/src/main/java/com/example/socialapp/model/User.java
+++ InstantMessengerBackend/src/main/java/com/example/socialapp/model/User.java
@@ -1,11 +1,142 @@
 package com.example.socialapp.model;
 
 import javax.persistence.*;
+import javax.sql.rowset.serial.SerialBlob;
+import java.text.SimpleDateFormat;
 
 @Entity
-@Table(name = "users")
+//@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+    private String firstName;
+    private String lastName;
+
+    @Column(unique=true)
+    private String mail;
+    private String password;
+    private boolean active;
+    private String roles;
+
+    private String country;
+    private String city;
+    private SimpleDateFormat birthday;
+    private String gender;
+    private String phone;
+    private String aboutMe;
+    private SerialBlob profileImage;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public SimpleDateFormat getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(SimpleDateFormat birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAboutMe() {
+        return aboutMe;
+    }
+
+    public void setAboutMe(String aboutMe) {
+        this.aboutMe = aboutMe;
+    }
+
+    public SerialBlob getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(SerialBlob profileImage) {
+        this.profileImage = profileImage;
+    }
+
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
 }
