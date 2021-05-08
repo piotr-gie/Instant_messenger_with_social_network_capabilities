183e3fef7bf734194c141cbe0d0a21ad344e394e
dzeju <dzeju@users.noreply.github.com>
2021-05-08 14:29:15 +0200

    suggestions in dropdown

--- InstantMessengerFrontend/src/app/components/search/search.component.ts
+++ InstantMessengerFrontend/src/app/components/search/search.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
+import { GenderType } from 'src/app/enums/gender-type.enum';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
+import { from, Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
@@ -8,11 +14,37 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
   show: boolean;
+  users: User [] = [];
+  usersSearchList: User [] = [];
+  // users: Observable<User>
+  usersSearch: Observable<User>
+  genderType = GenderType;
+  avatar: File;
 
-
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
+    this.initializeUserList();
   }
 
+  initializeUserList() {
+    this.userService.getModels().subscribe((response) => {
+      this.users = response;
+    })
+  }
+
+  showUserProfile(id: number) {
+    this.router.navigate(['/profile', id])
+  }
+
+  search($event){
+    let input = $event.target.value;
+    let userList: User [] = [];
+    const sauce = from(this.users);
+
+    sauce.pipe(filter(user => user.firstName.includes(input) || user.lastName.includes(input)))
+    .subscribe(us => userList.push(us));
+
+    this.usersSearchList = userList;
+  }
 }
